c03a8940c36f1a9a275a811d377317be3d153d3e
Umair Afzal <i190517@nu.edu.pk>
2021-11-28 09:49:44 +0500

    Added admin functions

--- src/main/java/GUIcode/ViewAdminDetails.java
+++ src/main/java/GUIcode/ViewAdminDetails.java
@@ -1,7 +1,9 @@
 package GUIcode;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -10,11 +12,19 @@ import java.util.ResourceBundle;
 public class ViewAdminDetails implements Initializable {
 
     @FXML
+    private Button Back;
+
+    @FXML
     private TextField password;
 
     @FXML
     private TextField username;
 
+    @FXML
+    void BackToMenu(ActionEvent event) {
+        HelloApplication.window.setScene(MainController.Adminsfunctionscene);
+        HelloApplication.window.show();
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         username.setText(MainController.flightReservationSystem.admin.getName());
